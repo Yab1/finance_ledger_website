fa54ec501902ba234a93d0dc79e3ca234b0d1320
Yab1 <yabilisanu@gmail.com>
2023-09-05 18:26:03 +0300

    Refactor: Imported section for rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+// Sections
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Cases from "./sections/Cases";
+import Blog from "./sections/Blog";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <About />
+      <Cases />
+      <Blog />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
