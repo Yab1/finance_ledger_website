8eaa223fb1ea2bdd75ee7772b6cf8c9ea78ead22
Yab1 <yabilisanu@gmail.com>
2023-09-05 18:22:40 +0300

    Added custom color and breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "320px",
+        md: "768px",
+        lg: "1360px",
+      },
+      colors: {
+        Green: "#28A745",
+        Blue: "#0284D0",
+        Dark: "#333333",
+        Creme: "#F4F4F4",
+      },
+    },
   },
   plugins: [],
 };
