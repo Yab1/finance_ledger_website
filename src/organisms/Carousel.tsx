d9c5c7884b651d652e05ddd8de53c7bebea9c6e9
Yab1 <yabilisanu@gmail.com>
2023-09-06 22:05:38 +0300

    Refactor: Created a carousel to show business cases

---
+++ src/organisms/Carousel.tsx
@@ -0,0 +1,40 @@
+// Bootstrap Icons
+import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
+
+//Assets
+import { cases } from "../store/IMAGES";
+
+type CarouselProps = {
+  currentCarousel: number;
+  open: boolean;
+  handleVisibility: () => void;
+  next: () => void;
+  prev: () => void;
+};
+
+function Carousel({
+  currentCarousel,
+  open,
+  handleVisibility,
+  next,
+  prev,
+}: CarouselProps) {
+  return open ? (
+    <div className="fixed inset-0 bg-backdrop text-white z-20 grid grid-cols-8 place-items-center">
+      <button className="col-start-1 md:col-start-2" onClick={prev}>
+        <ChevronLeft className="w-10 h-10" />
+      </button>
+      <img
+        src={cases[currentCarousel].image}
+        alt="cases[currentCarousel].image"
+        className="w-full col-start-2 col-span-6 rounded shadow-2xl md:col-start-3 md:col-span-4 transition-transform duration-500 transform hover:scale-105 cursor-pointer"
+        onClick={handleVisibility}
+      />
+      <button className="col-start-8 md:col-start-7" onClick={next}>
+        <ChevronRight className="w-10 h-10" />
+      </button>
+    </div>
+  ) : null;
+}
+
+export default Carousel;
