b8194530b3e804b280d9df5a333234dcd5bd32d0
Yab1 <yabilisanu@gmail.com>
2023-09-06 22:06:04 +0300

    Config: Removed background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,7 @@ export default {
         Blue: "#0284D0",
         Dark: "#333333",
         Creme: "#F4F4F4",
-      },
-      backgroundImage: {
-        "hero-mobile": "url('./src/assets/mobile/image-hero-mobile.svg')",
-        "hero-tablet": "url('./src/assets/tablet/image-hero-tablet.svg')",
-        "hero-desktop": "url('./image-hero-desktop.svg')",
+        backdrop: "rgba(0, 0, 0, 0.7)",
       },
     },
   },
